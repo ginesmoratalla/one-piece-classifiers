3c7d2eae962d0fb278f23d29ab0bf0d9f2ec2b38
Gines Moratalla <ginesmoratalla@gmail.com>
2025-06-19 19:51:57 +0200

    Images correctly loaded as a dataset in loader/

--- loader/dataloader.py
+++ loader/dataloader.py
@@ -4,20 +4,26 @@ from torch.utils.data import Dataset, DataLoader, TensorDataset
 import torchvision.transforms as transforms
 from sklearn.model_selection import train_test_split
 import numpy as np
+import os
+
+ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
+DATASET_PATH = os.path.join(ROOT_DIR, 'dataset/onepiecedata.pt')
 
 
 class OnePieceDataset(Dataset):
 
     def __init__(self):
-        xy = np.loadtxt()
-        self.batch_size = 16
-        self.x = torch.from_numpy(xy[:, 1:])
-        self.y = torch.from_numpy(xy[:, 0])
-        print(self.y)
-        self.num_samples = xy.shape[0]
+        xy = torch.load(DATASET_PATH)
+        self.x = torch.stack([tuple[0] for tuple in xy])
+        self.y = [tuple[1] for tuple in xy]
+
+        self.num_samples = len(self.y)
 
     def __getitem__(self, index):
         return self.x[index], self.y[index]
 
     def __len__(self):
         return self.num_samples
+
+
+dataset = OnePieceDataset()
--- utils/image_labels.py
+++ utils/image_labels.py
@@ -1,18 +1,21 @@
 import os
 import torch
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import cv2
+# import numpy as np
+# import matplotlib.pyplot as plt
 
+# --- Initial setup ---
 device = "mps"
 dataset = []
 IMG_SIZE = 150
 ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
 DATASET_PATH = os.path.join(ROOT_DIR, 'dataset/onepiecedata.pt')
 class_names = pd.read_csv(os.path.join(ROOT_DIR, 'dataset/classnames.csv'))
+# ---------------------
 
-# 18 classes of images (Characters)
+
+# -------- 18 Image Classes --------
 for idx, label in enumerate(class_names['Class']):
 
     print(f"Processing images for '{label}'")
@@ -22,6 +25,8 @@ for idx, label in enumerate(class_names['Class']):
 
         raw_img = cv2.imread(os.path.join(character_path, img))
         resized_img = cv2.resize(raw_img, (IMG_SIZE, IMG_SIZE))
-        dataset.append([torch.tensor(resized_img), torch.tensor(idx)])
+        image_tensor = torch.from_numpy(resized_img).permute(2, 0, 1)
+        dataset.append((image_tensor, idx))
 
-torch.save(dataset, DATASET_PATH)
+# ----------------------------------
+# torch.save(dataset, DATASET_PATH)
