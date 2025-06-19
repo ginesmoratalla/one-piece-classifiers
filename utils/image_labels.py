0d67e672e0473ba8d61bd8c99b18c4595da09539
Gines Moratalla <ginesmoratalla@gmail.com>
2025-06-19 18:25:40 +0200

    Script to save entries as a dataset

--- utils/image_labels.py
+++ utils/image_labels.py
@@ -2,11 +2,14 @@ import os
 import torch
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import cv2
 
+device = "mps"
 dataset = []
-IMG_SIZE = 100
+IMG_SIZE = 150
 ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
+DATASET_PATH = os.path.join(ROOT_DIR, 'dataset/onepiecedata.pt')
 class_names = pd.read_csv(os.path.join(ROOT_DIR, 'dataset/classnames.csv'))
 
 # 18 classes of images (Characters)
@@ -19,4 +22,6 @@ for idx, label in enumerate(class_names['Class']):
 
         raw_img = cv2.imread(os.path.join(character_path, img))
         resized_img = cv2.resize(raw_img, (IMG_SIZE, IMG_SIZE))
-        dataset.append([resized_img, idx])
+        dataset.append([torch.tensor(resized_img), torch.tensor(idx)])
+
+torch.save(dataset, DATASET_PATH)
